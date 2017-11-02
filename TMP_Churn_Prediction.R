7d80be8b09c58f07209343496498519e368cbcd0
MSTC Signal Processing and Machine Learning for Big Data <master.stc.ssr@gmail.com>
2017-11-02 18:09:04 +0100

    Update TMP_Churn_Prediction.R

--- TMP_Churn_Prediction.R
+++ TMP_Churn_Prediction.R
@@ -1,2 +1,9 @@
 file_train ='C:\\MSTC_BD\\NEW_2017_18\\CHURN\\DATA\\churn-bigml-80.csv'
 CV_data <- read.csv(file_train, header=TRUE, sep=",")
+
+summary(CV_data$State)
+
+windows()
+barplot(summary(CV_data$State))
+
+
